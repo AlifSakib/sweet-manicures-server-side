6d54928f2d0a258e6e81d2d79e382756e4c7b1df
AlifSakib <alifsakib@gmail.com>
2022-11-10 04:48:11 +0600

    aded redme file

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# b6a11-service-review-server-side-AlifSakib
+## Sweet Manicures
+
+# Live Site : https://dlux-inc.web.app/
+
+![Screenshot from 2022-11-07 14-57-00](https://i.ibb.co/zFtwkYf/Sweet-Menicure.png)
+
+### Features
+
+- Jwt Authorization
+- Firebase Authentication
+- Private Routes
+- Add Review
+- Delete Review
+- Edit Review
+- Add Services
+- Social Login
+- Data stored in MongoDB
+- Node Server
+- Responsive
+
+Still in development phase more features will add in future like
+
+- admin dashboard,
+- User Profile
+- Activity Graph etc
+
+### Resources
+
+- [Tailwind CSS](https://tailwindcss.com)
+- [Unsplash](https://unsplash.com)
+- [ImageBB](https://imgbb.com/)
+- [Tailblock](https://tailblocks.cc/)
+- [LottieFiles](https://lottiefiles.com/)
+- [Kitwind](https://kitwind.io/)
+
+### Credits
+
+- [Alif Sakib](https://www.linkedin.com/in/alif-sakib-224002207/)
+
+### Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/)
+
+- The site was developed for learning purpose . Feel free contribute :)
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 require("colors");
 const jwt = require("jsonwebtoken");
 
-const port = process.send.PORT || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -58,30 +58,30 @@ app.get("/services", async (req, res) => {
     const query = {};
     const cursor = serviceCollection.find(query).sort({ _id: -1 });
     const services = await cursor.limit(3).toArray();
-    res.json({
+    res.send({
       success: true,
       data: services,
     });
   } catch (error) {
-    res.json({
+    res.send({
       success: false,
     });
   }
 });
 
-app.get("/services/all", async (req, res) => {
+app.get("/allservices", async (req, res) => {
   try {
     const query = {};
     const cursor = serviceCollection.find(query);
     const services = await cursor.toArray();
     const count = await serviceCollection.estimatedDocumentCount();
-    res.json({
+    res.send({
       success: true,
       data: services,
       count: count,
     });
   } catch (error) {
-    res.json({
+    res.send({
       success: false,
     });
   }
@@ -91,7 +91,7 @@ app.post("/services", async (req, res) => {
   try {
     const service = req.body;
     const result = await serviceCollection.insertOne(service);
-    res.json({
+    res.send({
       success: true,
     });
   } catch (error) {
@@ -104,7 +104,7 @@ app.get("/services/:id", async (req, res) => {
     const id = req.params.id;
     const query = { _id: ObjectId(id) };
     const service = await serviceCollection.findOne(query);
-    res.json({
+    res.send({
       success: true,
       data: service,
     });
@@ -118,12 +118,12 @@ app.post("/reviews", async (req, res) => {
   try {
     const review = req.body;
     const result = await reviewCollections.insertOne(review);
-    res.json({
+    res.send({
       success: true,
       data: result,
     });
   } catch (error) {
-    res.json({
+    res.send({
       success: false,
     });
   }
@@ -135,12 +135,12 @@ app.get("/reviews/:id", async (req, res) => {
     const query = { service_id: id };
     const cursor = reviewCollections.find(query).sort({ _id: -1 });
     const reviews = await cursor.toArray();
-    res.json({
+    res.send({
       success: true,
       data: reviews,
     });
   } catch (error) {
-    res.json({
+    res.send({
       success: false,
     });
   }
@@ -161,7 +161,7 @@ app.get("/reviews", verifyJWT, async (req, res) => {
 
     const cursor = reviewCollections.find(query);
     const reviews = await cursor.toArray();
-    res.json({
+    res.send({
       success: true,
       data: reviews,
     });
@@ -175,12 +175,12 @@ app.delete("/reviews/:id", async (req, res) => {
     const id = req.params.id;
     const query = { _id: ObjectId(id) };
     const result = await reviewCollections.deleteOne(query);
-    res.json({
+    res.send({
       success: true,
       data: result,
     });
   } catch (error) {
-    res.json({
+    res.send({
       success: false,
     });
   }
@@ -209,11 +209,11 @@ app.patch("/editReview/:id", async (req, res) => {
       updateReview,
       option
     );
-    res.json({
+    res.send({
       success: true,
     });
   } catch (error) {
-    res.json({
+    res.send({
       success: false,
     });
   }
